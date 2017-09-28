cd30ee405c1765eb3fc6437be794109a19682eeb
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-09-28 12:08:22 +0800

    Update React Redux Files

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import { createStore, applyMiddleware} from 'redux';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
-import promise from 'redux-promise-middleware';
+import promiseMiddleware from 'redux-promise-middleware';
+import { createLogger } from 'redux-logger';
 import reducers from '../reducers/index';
 
 const store = createStore(
   reducers,
-  applyMiddleware(createLogger(), thunk, promise())
+  applyMiddleware(
+    thunk,
+    promiseMiddleware(),
+    createLogger()
+  )
 );
 
 export default store;
