3e97612d2e884981edea4417d122d14fc0ec3bf9
Jovani Cadorna Warguez <jovaniwarguez21@gmail.com>
2017-09-29 22:07:17 +0800

    Update Assets Lib

--- lib/assets.php
+++ lib/assets.php
@@ -1,14 +1,17 @@
 <?php
+
 function assets() {
 
-	// CSS
-	wp_enqueue_style( 'main-css', get_template_directory_uri().'/dist/styles/main.css', false, null);
+  // Fonts
+  wp_enqueue_style( 'google_font/lobster', 'https://fonts.googleapis.com/css?family=Lobster' );
+  wp_enqueue_style( 'google_font/lato', 'https://fonts.googleapis.com/css?family=Lato' );
 
-	// JS
-	wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/dist/scripts/main.js', array(), null, true);
-	wp_enqueue_script( 'react-redux-js', get_stylesheet_directory_uri() . '/dist/build/bundle.js', array(), null, true);
+  // Stylesheets
+  wp_enqueue_style( 'main-css', get_template_directory_uri().'/dist/styles/main.css', false, null);
+
+  // Scripts
+  wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/dist/scripts/main.js', array(), null, true);
+  wp_enqueue_script( 'react-redux-js', get_stylesheet_directory_uri() . '/dist/build/bundle.js', array(), null, true);
 }
+
 add_action( 'wp_enqueue_scripts', 'assets' );
-
-
-?>
