f609f037343506c4a79f2eca26915ecf212f2cae
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-11-17 12:16:10 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,16 +1,20 @@
-<?
-$sage_includes = [
-  'lib/assets.php',
-  'lib/extras.php',
-  'lib/init.php'
+<?php
+$libraries = [
+  'lib/init.php',
+  'lib/assets.php'
 ];
 
-foreach ($sage_includes as $file) {
-  if (!$filepath = locate_template($file)) {
-    trigger_error(sprintf(__('Error locating %s for inclusion', 'sage'), $file), E_USER_ERROR);
+foreach ( $libraries as $lib ) {
+  $file_path = locate_template($lib);
+
+  if ( !$file_path ) {
+    trigger_error(
+      sprintf('%s Not found', $lib), E_USER_ERROR
+    );
   }
 
-  require_once $filepath;
+  require_once $file_path;
 }
-unset($file, $filepath);
+
+unset($lib, $libraries);
 ?>
