afc951ca8cdc1f137f5ce9906651e9903d6d8d0b
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-11-17 16:17:32 +0800

    Update Assets Lib

--- lib/assets.php
+++ lib/assets.php
@@ -6,13 +6,15 @@ function assets() {
   wp_enqueue_style( 'lobster-font', 'https://fonts.googleapis.com/css?family=Lobster' );
   wp_enqueue_style( 'lato-font', 'https://fonts.googleapis.com/css?family=Lato' );
 
-  // Stylesheets
-  wp_enqueue_style( 'main-css', get_template_directory_uri().'/dist/styles/main.css', false, null);
+  // Styles
+  wp_enqueue_style( 'vendor-css', get_stylesheet_directory_uri() . '/dist/styles/vendor.min.css', false, null);
+  wp_enqueue_style( 'main-css', get_template_directory_uri() . '/dist/styles/main.min.css', false, null);
 
   // Scripts
-  wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/dist/scripts/main.js', array(), null, true);
+  wp_enqueue_script( 'vendor-js', get_stylesheet_directory_uri() . '/dist/scripts/vendor.min.js', array(), null, true);
+  wp_enqueue_script( 'main-js', get_stylesheet_directory_uri() . '/dist/scripts/main.min.js', array(), null, true);
   wp_enqueue_script( 'react-redux-js', get_stylesheet_directory_uri() . '/dist/build/bundle.js', array(), null, true);
-  wp_localize_script( 'react-redux-js', 'WP_REACT_REDUX', array(
+  wp_localize_script( 'react-redux-js', 'JOVANI_CODES', array(
     'siteName'        => get_bloginfo( 'name' ),
     'siteDescription' => get_bloginfo( 'description' ),
     'images'          => get_template_directory_uri() . '/dist/images',
