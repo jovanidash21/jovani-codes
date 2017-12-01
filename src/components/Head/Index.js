03e2354902a9baee8393138d1638436c9385de4a
Jovani Cadorna Warguez <jwardash21@gmail.com>
2017-12-01 20:25:26 +0800

    Update Head Component

--- src/components/Head/Index.js
+++ src/components/Head/Index.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from "react-helmet";
 
-class Head extends Component {
-  render () {
-    const {
-      title,
-      description,
-      keywords
-    } = this.props;
-
-    return (
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="keywords" content={keywords} />
-      </Helmet>
-    );
-  }
-};
+const Head = (props) => {
+  return (
+    <Helmet>
+      <title>{props.title}</title>
+      <meta name="description" content={props.description} />
+      <meta name="keywords" content={props.keywords} />
+    </Helmet>
+  );
+}
 
 Head.propTypes = {
   title: PropTypes.string.isRequired,
