b86f9648174058fc84181c14a1ac6bff48a5d89b
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-03-18 21:19:40 +0800

    Update Banner Section

--- src/containers/Partial/Banner.js
+++ src/containers/Partial/Banner.js
@@ -29,106 +29,110 @@ class Banner extends Component {
           params={particlesJSConfig}
           className="particles"
         />
-        <FaceLogo
-          className="main-logo"
-          width="120px"
-        />
-        <div className="banner-description">
-          <Container fluid>
-            <Row>
-              <Col xs="12" sm="8" md="7">
-                <div className={"animated " + (isActive ? 'slideInLeft' : 'slideOutLeft')}>
-                  <FullName />
-                </div>
-              </Col>
-            </Row>
-          </Container>
+        <div className="main-logo-wrapper">
+          <div className={"animated " + (isActive ? 'slideInDown' : 'slideOutUp')}>
+            <FaceLogo
+              className="main-logo"
+              width="120px"
+            />
+          </div>
         </div>
-        <div className={"animated " + (isActive ? 'slideInRight' : 'slideOutRight')}>
-          <FacePortrait />
+        <div className="full-name-wrapper">
+          <div className={"animated " + (isActive ? 'slideInLeft' : 'slideOutLeft')}>
+            <FullName />
+          </div>
+        </div>
+        <div className="face-portrait-wrapper">
+          <div className={"animated " + (isActive ? 'slideInRight' : 'slideOutRight')}>
+            <FacePortrait />
+          </div>
         </div>
         <div className="social-media-menu">
           <div className={"animated " + (isActive ? 'zoomIn' : 'zoomOut')}>
-          {
-            options.data.acf.social_medias &&
-            options.data.acf.social_medias.map((socialMedia, i) => {
-              switch (socialMedia) {
-                case 'facebook':
-                  return (
-                    <SocialMediaItem
-                      socialMedia="facebook"
-                      icon="facebook"
-                      link={options.data.acf.facebook_url}
-                      title={options.data.acf.facebook_message}
-                    />
-                  );
-                  break;
-                case 'twitter':
-                  return (
-                    <SocialMediaItem
-                      socialMedia="twitter"
-                      icon="twitter"
-                      link={options.data.acf.twitter_url}
-                      title={options.data.acf.twitter_message}
-                    />
-                  );
-                  break;
-                case 'instagram':
-                  return (
-                    <SocialMediaItem
-                      socialMedia="instagram"
-                      icon="instagram"
-                      link={options.data.acf.instagram_url}
-                      title={options.data.acf.instagram_message}
-                    />
-                  );
-                  break;
-                case 'linkedin':
-                  return (
-                    <SocialMediaItem
-                      socialMedia="linkedin"
-                      icon="linkedin"
-                      link={options.data.acf.linkedin_url}
-                      title={options.data.acf.linkedin_message}
-                    />
-                  );
-                  break;
-                case 'google_plus':
-                  return (
-                    <SocialMediaItem
-                      socialMedia="google-plus"
-                      icon="google-plus"
-                      link={options.data.acf.google_plus_url}
-                      title={options.data.acf.google_plus_message}
-                    />
-                  );
-                  break;
-                case 'youtube':
-                  return (
-                    <SocialMediaItem
-                      socialMedia="youtube"
-                      icon="youtube-play"
-                      link={options.data.acf.youtube_url}
-                      title={options.data.acf.youtube_message}
-                    />
-                  );
-                  break;
-                case 'github':
-                  return (
-                    <SocialMediaItem
-                      socialMedia="github"
-                      icon="github"
-                      link={options.data.acf.github_url}
-                      title={options.data.acf.github_message}
-                    />
-                  );
-                  break;
+            {
+              options.data.acf.social_medias &&
+              options.data.acf.social_medias.map((socialMedia, i) => {
+                switch (socialMedia) {
+                  case 'facebook':
+                    return (
+                      <SocialMediaItem
+                        socialMedia="facebook"
+                        icon="facebook"
+                        link={options.data.acf.facebook_url}
+                        title={options.data.acf.facebook_message}
+                      />
+                    );
+                    break;
+                  case 'twitter':
+                    return (
+                      <SocialMediaItem
+                        socialMedia="twitter"
+                        icon="twitter"
+                        link={options.data.acf.twitter_url}
+                        title={options.data.acf.twitter_message}
+                      />
+                    );
+                    break;
+                  case 'instagram':
+                    return (
+                      <SocialMediaItem
+                        socialMedia="instagram"
+                        icon="instagram"
+                        link={options.data.acf.instagram_url}
+                        title={options.data.acf.instagram_message}
+                      />
+                    );
+                    break;
+                  case 'linkedin':
+                    return (
+                      <SocialMediaItem
+                        socialMedia="linkedin"
+                        icon="linkedin"
+                        link={options.data.acf.linkedin_url}
+                        title={options.data.acf.linkedin_message}
+                      />
+                    );
+                    break;
+                  case 'google_plus':
+                    return (
+                      <SocialMediaItem
+                        socialMedia="google-plus"
+                        icon="google-plus"
+                        link={options.data.acf.google_plus_url}
+                        title={options.data.acf.google_plus_message}
+                      />
+                    );
+                    break;
+                  case 'youtube':
+                    return (
+                      <SocialMediaItem
+                        socialMedia="youtube"
+                        icon="youtube-play"
+                        link={options.data.acf.youtube_url}
+                        title={options.data.acf.youtube_message}
+                      />
+                    );
+                    break;
+                  case 'github':
+                    return (
+                      <SocialMediaItem
+                        socialMedia="github"
+                        icon="github"
+                        link={options.data.acf.github_url}
+                        title={options.data.acf.github_message}
+                      />
+                    );
+                    break;
                 }
-            })
-          }
+              })
+            }
           </div>
         </div>
-        <ScrollDownIcon title="Scroll Down" />
+        <div className="scroll-down-icon-wrapper">
+          <div className={"animated " + (isActive ? 'slideInUp' : 'slideOutDown')}>
+            <ScrollDownIcon title="Scroll Down" />
+          </div>
+        </div>
       </section>
     )
   }
