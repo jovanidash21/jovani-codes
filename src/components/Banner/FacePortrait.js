558c9c9fdb1e9450ac784236869c8bcb3d4a9fb9
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-03-20 19:57:47 +0800

    Update Face Portrait Component

--- src/components/Banner/FacePortrait.js
+++ src/components/Banner/FacePortrait.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import VelocityTransitionGroup from 'velocity-react/velocity-transition-group';
 import Typist from 'react-typist';
 
 class FacePortrait extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      showSpeechBubble: false
+    };
+  }
+  componentDidMount() {
+    this.interval = setInterval(::this.handleSpeechBubbleToggle, 10000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  handleSpeechBubbleToggle() {
+    const { showSpeechBubble } = this.state;
+
+    this.setState({showSpeechBubble: !showSpeechBubble});
   }
   render() {
     const { speech } = this.props;
+    const { showSpeechBubble } = this.state;
 
     return (
       <div className="face-portrait">
-        <div className="speech-bubble big right d-none d-md-block">
-          <Typist>
-            <Typist.Delay ms={500} />
-            {speech}
-          </Typist>
-        </div>
+      <VelocityTransitionGroup enter={{animation: "fadeIn"}} leave={{animation: "fadeOut"}}>
+        {
+          showSpeechBubble &&
+          <div className="speech-bubble big right">
+            <Typist>
+              <Typist.Delay ms={500} />
+              {speech}
+            </Typist>
+          </div>
+        }
+      </VelocityTransitionGroup>
         <img src={JOVANI_CODES.images + '/low-poly-portrait.png'} className="low-poly" />
       </div>
     )
