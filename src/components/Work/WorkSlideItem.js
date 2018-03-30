0b60559490a10cc3e745240d0d5262a08aae0319
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-03-30 15:09:46 +0800

    Add Animations On Work Section

--- src/components/Work/WorkSlideItem.js
+++ src/components/Work/WorkSlideItem.js
@@ -8,20 +8,28 @@ const WorkSlideItem = (props) => {
       <Container>
         <Row>
           <Col className="work-image-wrapper" xs="12" md="7">
-            <img src={JOVANI_CODES.images + '/work/desktop.png'} className="img-fluid work-image" />
+            <img src={props.image} className="img-fluid work-image" />
           </Col>
           <Col className="work-description-wrapper" xs="12" md="5">
             <div className="work-description">
-              <h2 className="section-title">{props.title}</h2>
+              <h2 className={"section-title animated " +
+                (props.isSlideActive ? 'fadeInUp' : 'fadeOutDown')}
+              >
+                {props.title}
+              </h2>
               {
                 props.description &&
-                <p className="section-description">
+                <p className={"section-description animated " +
+                  (props.isSlideActive ? 'fadeIn' : 'fadeOut')}
+                >
                   {props.description}
                 </p>
               }
               {
                 props.link &&
-                <div className="flip-button">
+                <div className={"flip-button animated " +
+                  (props.isSlideActive ? 'zoomIn' : 'zoomOut')}
+                >
                   <a href={props.link} target="_blank" title="Visit the site" />
                 </div>
               }
@@ -35,13 +43,16 @@ const WorkSlideItem = (props) => {
 
 WorkSlideItem.propTypes = {
   title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
   description: PropTypes.string,
-  link: PropTypes.string
+  link: PropTypes.string,
+  isSlideActive: PropTypes.bool,
 }
 
 WorkSlideItem.defaultProps = {
   description: '',
-  link: ''
+  link: '',
+  isSlideActive: false
 }
 
 export default WorkSlideItem;
--- src/containers/Partial/Work.js
+++ src/containers/Partial/Work.js
@@ -19,6 +19,7 @@ class Work extends Component {
   }
   render() {
     const { isSectionActive } = this.props;
+    const { activeSlide } = this.state;
     const slickOptions = {
       dots: true,
       infinite: true,
@@ -27,51 +28,88 @@ class Work extends Component {
       slidesToScroll: 1,
       afterChange: ::this.handleAfterChange
     };
+    const workImagePath = JOVANI_CODES.images + '/work/';
 
     return (
       <section className="work-section background-secondary">
         <Slider {...slickOptions}>
           <div className="work-content">
-            <WorkSlideItem
-              title="Work 1"
-              description="Quisque velit nisi, pretium ut lacinia in, elementum id enim."
-              link="http://example.com"
-            />
+            <div className={"animated " +
+              (isSectionActive ? 'fadeInUpBig' : 'fadeOutDownBig')}
+            >
+              <WorkSlideItem
+                title="Work 1"
+                image={workImagePath + 'laptop.png'}
+                description="Quisque velit nisi, pretium ut lacinia in, elementum id enim."
+                link="http://example.com"
+                isSlideActive={activeSlide === 0}
+              />
+            </div>
           </div>
           <div className="work-content">
-            <WorkSlideItem
-              title="Work 2"
-              description="Nulla quis lorem ut libero malesuada feugiat."
-              link="http://example.com"
-            />
+            <div className={"animated " +
+              (isSectionActive ? 'fadeInUpBig' : 'fadeOutDownBig')}
+            >
+              <WorkSlideItem
+                image={workImagePath + 'desktop.png'}
+                title="Work 2"
+                description="Nulla quis lorem ut libero malesuada feugiat."
+                link="http://example.com"
+                isSlideActive={activeSlide === 1}
+              />
+            </div>
           </div>
           <div className="work-content">
-            <WorkSlideItem
-              title="Work 3"
-              description="Vivamus suscipit tortor eget felis porttitor volutpat."
-              link="http://example.com"
-            />
+            <div className={"animated " +
+              (isSectionActive ? 'fadeInUpBig' : 'fadeOutDownBig')}
+            >
+              <WorkSlideItem
+                image={workImagePath + 'tablet.png'}
+                  title="Work 3"
+                  description="Vivamus suscipit tortor eget felis porttitor volutpat."
+                  link="http://example.com"
+                  isSlideActive={activeSlide === 2}
+              />
+            </div>
           </div>
           <div className="work-content">
-            <WorkSlideItem
-              title="Work 4"
-              description="Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui."
-              link="http://example.com"
-            />
+            <div className={"animated " +
+              (isSectionActive ? 'fadeInUpBig' : 'fadeOutDownBig')}
+            >
+              <WorkSlideItem
+                image={workImagePath + 'laptop.png'}
+                  title="Work 4"
+                  description="Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui."
+                  link="http://example.com"
+                isSlideActive={activeSlide === 3}
+              />
+            </div>
           </div>
           <div className="work-content">
-            <WorkSlideItem
-              title="Work 5"
-              description="Cras ultricies ligula sed magna dictum porta."
-              link="http://example.com"
-            />
+            <div className={"animated " +
+              (isSectionActive ? 'fadeInUpBig' : 'fadeOutDownBig')}
+            >
+              <WorkSlideItem
+                image={workImagePath + 'desktop.png'}
+                  title="Work 5"
+                  description="Cras ultricies ligula sed magna dictum porta."
+                  link="http://example.com"
+                  isSlideActive={activeSlide === 4}
+              />
+            </div>
           </div>
           <div className="work-content">
-            <WorkSlideItem
-              title="Work 6"
-              description="Donec sollicitudin molestie malesuada."
-              link="http://example.com"
-            />
+            <div className={"animated " +
+              (isSectionActive ? 'fadeInUpBig' : 'fadeOutDownBig')}
+            >
+              <WorkSlideItem
+                image={workImagePath + 'tablet.png'}
+                title="Work 6"
+                description="Donec sollicitudin molestie malesuada."
+                link="http://example.com"
+                isSlideActive={activeSlide === 5}
+              />
+            </div>
           </div>
         </Slider>
       </section>
