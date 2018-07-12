a342509d03512497c99306c2236fce015cd6714c
Jovani Cadorna Warguez <jwardash21@gmail.com>
2018-07-12 13:16:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <?php wp_head(); ?>
     <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri() . '/favicons/apple-touch-icon.png' ?>">
     <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri() . '/favicons/favicon-32x32.png' ?>">
